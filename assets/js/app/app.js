b6fedc2686c54585065fb674237d90be505519eb
Francesc <fpbosch@gmail.com>
2014-07-30 16:31:36 +0200

    Comented services, filters and directives in the app.js

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -2,15 +2,17 @@ define([
       'angular',
       'angular.route', 
       './controllers/index',
-      './services/index',
-      './directives/index',
-      './filters/index'
+      /*'./services/index',*/
+      /*'./directives/index',*/
+      /*'./filters/index'*/
   ], function (ng) {
       'use strict';
     
      return ng.module('app', [
          'ngRoute',
          'app.controllers',
-         'app.services'
+         // call your 'app.services',
+         // call your 'app.directives',
+         // call your 'app.filters',
      ]);
 });
